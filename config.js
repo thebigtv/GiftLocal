593a6eff4d7790443dbb6ff3dd32362f2defecf1
Gary <garypw81@yahoo.com>
2026-07-02 21:12:18 +0100

    Update config.js

--- config.js
+++ config.js
@@ -2,4 +2,4 @@
 // Paste your Apps Script Web App URL here after each new deployment.
 // Deploy > Manage deployments > copy the URL ending in /exec
 
-const API_URL = 'PASTE_YOUR_WEB_APP_URL_HERE';
+const API_URL = 'https://script.google.com/macros/s/AKfycbx0WU7d2BD_vu7Lv82BMqN2op_OGdlvklLmaBF2J9Ej-MVbtuLQqy1QrxuU8QwKkU7d/exec';
